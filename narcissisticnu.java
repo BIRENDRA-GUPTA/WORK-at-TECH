c8d4ef25bb0f889e31ab59939511cdec5f896a73
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-02 05:47:17 +0530

    time is over

---
+++ narcissisticnu.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class narcissisticnu {
+    public static void main(String[] args) {
+        Scanner biru = new Scanner(System.in);
+		int t = biru.nextInt();
+		for(int i = 0; i < t; i++){
+			int n = biru.nextInt();
+			int num = n;
+			int sum = 0;
+			int numDigits =(int) Math.log10(n) + 1;
+			while (num > 0){
+				int digit = num % 10;
+				sum += Math.pow(digit,numDigits);
+				num /= 10;
+			}
+			if (sum == n){
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+			
+		}
+		biru.close();
+    }
+}
+
