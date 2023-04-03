d9918a3016e014beb00faa51381aad5a82ec2caf
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-04 05:03:02 +0530

    practice

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class helloworld {
+    public static void main(String[] args) {
+        Scanner biru = new Scanner(System.in);
+        int n = biru.nextInt();
+        for (int i = 0; i < n; i++){
+            System.out.println("RAHUL");
+            System.out.println("     rahul");
+            System.out.println("          RAHUL");
+        }
+        biru.close();
+    }
+
+}
