61782107a9da15fb01aa7e9a7a39b02cc534ab97
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-04 05:53:38 +0530

    Hello

---
+++ time_complexity.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class time_complexity {
+    public static void main(String[] args) {
+        Scanner biru = new Scanner(System.in);
+        int n = biru.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.println("HELLO");
+            }
+        }
+        biru.close();
+    }
+}
