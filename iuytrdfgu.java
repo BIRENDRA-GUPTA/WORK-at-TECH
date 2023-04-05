f10adac645e0a57ece1f52b75dddc5345bbd8930
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-05 20:05:10 +0530

    hlo

---
+++ iuytrdfgu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class iuytrdfgu {
+    public static void main(String[] args) {
+        Scanner biru = new Scanner(System.in);
+	    int n = biru.nextInt();
+	    for (int i = 0 ; i < n; i++){ 
+		System.out.println("GfG!");
+	    }
+	    biru.close();   
+    }
+}
