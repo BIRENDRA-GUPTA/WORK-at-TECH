dc9547a7b4aa3a66b0cae6ec7094705dea384229
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-16 00:01:18 +0530

    scan name

---
+++ sgdvhcbdjks.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class sgdvhcbdjks {
+    public static void main(String[] args) {
+        Scanner biru = new Scanner(System.in);
+        String n = biru.next();
+        System.out.println(n);
+    }
+}
