1a041cbcd8a071296cc7c7f6e24b642c4ac1dd55
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-04-30 14:01:24 +0530

    searching

---
+++ insert_position_in_sorted_array_searching.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class insert_position_in_sorted_array_searching {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int key = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            int index = binarySearch(arr, key);
+            System.out.println(index);
+        }
+    }
+
+    // function to perform binary search
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // key not found, return the index to insert the key
+        return left;
+    }
+}
