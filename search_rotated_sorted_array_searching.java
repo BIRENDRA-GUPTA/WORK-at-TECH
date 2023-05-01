8b82698af47353012f7dacc63cfd054e25d2e637
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-05-01 19:56:28 +0530

    searhing

---
+++ search_rotated_sorted_array_searching.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class search_rotated_sorted_array_searching {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int target = sc.nextInt();
+            int result = search(arr, target);
+            System.out.println(result);
+        }
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[lo] <= nums[mid]) {
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+        }
+        return -1;   
+    }
+}
