87e52d602f3d7e1cee59de5bd7c848bd3747f180
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-05-13 21:42:33 +0530

    two pinter

---
+++ two_sum_sorted_two_pointer.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class two_sum_sorted_two_pointer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            boolean result = hasZeroSumPair(arr);
+            System.out.println(result);
+        }
+    }
+
+    public static boolean hasZeroSumPair(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == 0) {
+                return true;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return false;
+    }
+}
