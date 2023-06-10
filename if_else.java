0110face35a2d960afcbf64df80f42dd28512cba
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-06-11 04:37:14 +0530

    practice

--- if_else.java
+++ if_else.java
@@ -1,13 +1,44 @@
 public class if_else {
-    public static void main(String[] args) {
-        char grade = 'D';
-        if (grade == 'B'){
-        System.out.println("your grade is good");
+    public static int main(String[] args) {
+        // char grade = 'E';
+        // if (grade == 'B'){
+        // System.out.println("your grade is good");
+        // }else if (grade == 'E'){
+        //     System.out.println("my gyrade is normal");
+        // }
+        // else if (grade == 'C'){
+        //     System.out.println("your grade is ok");
+        // }if(grade == 'D'){
+        //     System.out.println("grade is very poor");
+        // }
+
+
+    //  int a = 23;
+    //  while(true){
+    //     System.out.println(a);
+    //     a++;
+    //     if(a<100){
+    //         break;
+    //     }
+    //  } 
+
+
+    // int marks[] = new int[5];
+    // marks[0] = 60;
+    // marks[1] = 70;
+    // marks[2] = 80;
+    // marks[3] = 90;
+    // marks[4] = 100;
+    // System.out.println(marks[4]);    
+    
+    // int marks[] = {10,20,30,40,50,60,70,80,90};
+
+    // for (int i = 0; i<marks.length; i++){
+    //     System.out.println(marks[i]);
+    // }
+
+\
         }
-        else if (grade == 'C'){
-            System.out.println("your grade is ok");
-        }if(grade == 'D'){
-            System.out.println("grade is very poor");
-        }
+
     }
-}
+
