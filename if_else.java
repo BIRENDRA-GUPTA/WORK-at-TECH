90a2884024b9989c2e6a3be1f9c18973668e5b02
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-06-11 00:55:17 +0530

    practice

---
+++ if_else.java
@@ -0,0 +1,13 @@
+public class if_else {
+    public static void main(String[] args) {
+        char grade = 'D';
+        if (grade == 'B'){
+        System.out.println("your grade is good");
+        }
+        else if (grade == 'C'){
+            System.out.println("your grade is ok");
+        }if(grade == 'D'){
+            System.out.println("grade is very poor");
+        }
+    }
+}
