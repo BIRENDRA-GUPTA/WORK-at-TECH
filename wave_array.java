2f5ab18bcb4f59b172a697cf22946ffdb827d84e
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-06-16 00:06:22 +0530

    array

---
+++ wave_array.java
@@ -0,0 +1,10 @@
+public class wave_array {
+     public static void convertToWave(int n, int[] arr) {
+        // code here
+        for(int i=0;i<n-1;i=i+2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+}
+}
