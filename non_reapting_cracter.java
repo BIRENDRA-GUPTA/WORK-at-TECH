fa797c1a56063c76c8ae1f64368a73050469e0df
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-06-21 21:48:43 +0530

    string

--- non_reapting_cracter.java
+++ non_reapting_cracter.java
@@ -1,3 +1,28 @@
 public class non_reapting_cracter {
-    
+     public static void main(String[] args) {
+        String S = "hello";
+        char result = nonrepeatingCharacter(S);
+        System.out.println(result);
+    }
+
+    public static char nonrepeatingCharacter(String S) {
+        int[] count = new int[26];
+
+        // Step 1: Count the occurrences of each character
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            count[ch - 'a']++;
+        }
+
+        // Step 2: Find the first non-repeating character
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            if (count[ch - 'a'] == 1) {
+                return ch;
+            }
+        }
+
+        // Step 3: Return '$' if no non-repeating character is found
+        return '$';
+    }
 }
