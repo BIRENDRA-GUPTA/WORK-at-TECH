d7e2163283d3f3362714d2928ea9fe31c7eec113
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-07-02 11:34:29 +0530

    practice

---
+++ constructore_practice.java
@@ -0,0 +1,12 @@
+class Person{
+    String name;
+Person(){
+    System.out.println("constructor called");
+}
+}
+public class constructore_practice {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+       System.out.println(p1);
+    }
+}
