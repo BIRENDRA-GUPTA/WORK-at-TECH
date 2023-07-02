82eacaec201ede02f60a3044ad52c026826e1258
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-07-02 11:45:38 +0530

    this practice

---
+++ thise.java
@@ -0,0 +1,12 @@
+class Person{
+    String name = "biru";
+    Person(){
+        System.out.println(this.name);
+    }
+}
+public class thise {
+    public static void main(String[] args) {
+        Person p2 = new Person();
+        
+    }
+}
